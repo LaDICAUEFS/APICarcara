afda8991ebaf7f1e7e1153b79108e996717197f0
Marcos Ramos <mjramosfsa@gmail.com>
2020-06-09 11:11:49 -0300

    correções

--- api/index.js
+++ api/index.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.set('view engine','ejs');
-app.use(express.static('public'));
 
 app.get("/",(req,res)=>{
-    res.render("index.ejs");
+    res.json({
+        teste:"teste"
+    })
 });
 
 app.post("/",(req, res) => {
-     res.json({
-         teste:"teste"
-     })
+  
 });
 app.listen(3200,()=>{console.log("Api em execução!");});
