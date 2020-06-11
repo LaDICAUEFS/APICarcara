dd8978abc3791739a62db95d02119e60e0cd3c1d
Marcos Ramos <mjramosfsa@gmail.com>
2020-06-11 19:56:42 -0300

    tentativa de  escrita  e aquivo txt porem está erro e  erro na   interface

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const axios = require("axios");
+const fs = require("fs");
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:false}));
@@ -10,18 +11,35 @@ app.use(bodyParser.json());
 
 app.get("/",(req,res)=>{
 
-    let ab = inputOutput.read() ;
-
     res.json({
-                                    // o json  com os os dados 
+            teste:"teste"                   // o json  com os os dados 
     });
-    res.statusCode(200);
+    res.status(200);
 });
 
 app.post("/",(req, res) => {
 
-    let ab = inputOutput.write() ;
+         res.send("teste!");
+         res.status(200);
+    /*fs.writeFile('./upload/message.txt', 'Hello World! \r \n',{enconding:'utf-8',flag: 'a'}, function (err) {
+        if (err) throw err;
+        console.log('Arquivo salvo!');
+    });
 
+    const calback = (err)=>{
+        if (err) throw err;
+        console.log('Arquivo salvo!');
+    }
+    res.statusCode= 200;
+
+    fs.writeFile("../upload/test.txt", "Hey there!","utf8", function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    
+        console.log("The file was saved!");
+    }); 
+   */
 });
 
 app.put("/:id",(req,res)=>{
